9d5c9b1b9f6d8195b9f8312ed8ff4869deb1be42
guiaf04 <guilhermearaujo@alu.ufc.br>
2024-10-11 21:11:29 -0300

    feat: add crawler for access basic TISS page

---
+++ src/main/groovy/aczg/TISSPage.groovy
@@ -0,0 +1,43 @@
+package aczg
+
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+
+class TISSPage {
+    def scraper
+
+    Document getPrestadoresPage(){
+        // Passo 1: Acessar "Espaço do Prestador de Serviços de Saúde"
+        scraper.updatePath('/ans/pt-br/assuntos/prestadores')
+
+        Document prestadorPage = scraper.getHtml()
+
+        if (!prestadorPage) {
+            println "Erro ao acessar a página de Espaço do Prestador de Serviços de Saúde"
+            return null
+        }
+
+        return prestadorPage
+    }
+
+    Document getTissPage(Document prestadorPage){
+        Element tissLink = prestadorPage.select("a:contains(TISS - Padrão para Troca de Informação de Saúde Suplementar)").first()
+
+        if (!tissLink) {
+            println "Link para TISS não encontrado"
+            return null
+        }
+
+        def tissPageLink = tissLink.attr("href").replace(scraper.getCompletePath(), "")
+        scraper.updatePath(tissPageLink)
+
+        Document tissPage = scraper.getHtml()
+
+        if (!tissPage) {
+            println "Erro ao acessar a página de TISS"
+            return null
+        }
+
+        return tissPage
+    }
+}
