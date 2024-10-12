914676e8c4a56bf32c43ad5dac73ccae0feef401
guiaf04 <guilhermearaujo@alu.ufc.br>
2024-10-11 21:13:44 -0300

    feat: add crawler for access relational TISS table page

---
+++ src/main/groovy/aczg/TISSRelationalTables.groovy
@@ -0,0 +1,32 @@
+package aczg
+
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+
+class TISSRelationalTables {
+    Scraper scraper
+
+    TISSRelationalTables(Scraper scraper) {
+        this.scraper = scraper
+    }
+
+    Document getRelationalTables(Document tissPage){
+        Element tissTable = tissPage.select("a:contains(Clique aqui para acessar as planilhas)").first()
+
+        if (!tissTable) {
+            println "Link para Tabelas Relacionadas não encontrado"
+            return null
+        }
+
+        String tissTableLink = tissTable.attr("href").replace(scraper.getCompletePath(), "")
+        scraper.updatePath(tissTableLink)
+
+        Document tissTablePage = scraper.getHtml()
+        if (!tissTablePage) {
+            println "Erro ao acessar a página de Tabelas Relacionadas TISS"
+            return null
+        }
+
+        return tissTablePage
+    }
+}
